028edb496131e8c5c63eb21695898c69bf61fddd
hzbhbz <121084351+hzbhbz@users.noreply.github.com>
2023-01-13 17:18:36 +0900

    Update SwaggerConfig.java

--- src/main/java/com/digital/config/SwaggerConfig.java
+++ src/main/java/com/digital/config/SwaggerConfig.java
@@ -69,7 +69,7 @@ public class SwaggerConfig {
 	
     private ApiInfo getApiInfo() {
         return new ApiInfoBuilder()
-                .title("commerce-auth-v1")
+                .title("commerce-auth-v2")
                 .build();
     }
 }
